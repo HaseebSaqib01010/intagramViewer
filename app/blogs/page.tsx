c457eb2be775a77002df5590db1a2e1c548cdd55
rohaanfarooq <farooqrohaan@gmail.com>
2024-12-10 01:02:58 +0500

    UPDATED: wrapped the content in AppWrapper and removed the ContentSection

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,30 +1,19 @@
-import { GetStaticProps } from 'next';
-import { blogs } from '@/data';
-import Blogs from '../../components/Blogs';
 import { FloatingNav } from '../../components/ui/FloatingNavbar';
-import ContentSection from '../../components/ui/ContentSection';
 import { navItems } from "@/data";
 import BlogDetails from '../../components/ui/BlogDetails';
 import Footer from '../../components/Footer';
+import AppWrapper from '@/components/AppWrapper';
 
 
 const BlogPage = () => {
   return (
-   
-    <>
-      <div style={{ marginTop: "180px" }}>
-        <ContentSection
-          title="Welcome to"
-          titleHighlight="Instagram Story"
-          content="Instagram stories have become a popular feature on the platform, allowing users to share photos and videos that disappear after 24 hours. "
-      
-        />
-     
-      </div>
-    <FloatingNav navItems={navItems} />
-    <BlogDetails/>
-    <Footer/>
-    </>
+    <AppWrapper>
+
+      <FloatingNav navItems={navItems} />
+      <BlogDetails />
+      <Footer />
+
+    </AppWrapper>
   );
 
 };
