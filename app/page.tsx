af0db4bf20922cbcbe873e3dc0a0d0396679e815
Haseeb Saqib <114069880+HaseebSaqib01010@users.noreply.github.com>
2024-12-14 15:02:00 +0500

    UPDATED: resolved build errors

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,8 @@ import ContentSection from "@/components/ui/ContentSection";
 import Link from "next/link";
 const theme = createTheme({});
 import InstagramStoryViewerFAQ from "../components/ui/FAQ";
-// import TranslationWrapper from "../components/TranslationWrapper";
 import i18n from '../i18n';
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider } from 'react-i18next';  
 const Home = () => {
   return (
     <I18nextProvider i18n={i18n}>
