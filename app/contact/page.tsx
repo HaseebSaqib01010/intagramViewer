a1c4e2065b1c38617a7ecb223281dd3e2e64f9f1
Haseeb Saqib <114069880+HaseebSaqib01010@users.noreply.github.com>
2024-12-19 01:02:09 +0500

    commented contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
-import { Flex} from '@mantine/core';
+import { Flex } from '@mantine/core';
 import AppWrapper from '@/components/AppWrapper';
 import MagicButton from '@/components/MagicButton';
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -13,88 +13,88 @@ import InstagramStoryViewer from "@/components/InstagramStoryViewer";
 import ContentSection from "@/components/ui/ContentSection";
 import Hero from '@/components/Hero';
 
-const formSchema = z.object({
-    email: z
-        .string()
-        .trim().email({ message: "Must be a valid email" }),
-    name: z.string().trim().min(2, { message: "Name must be at least 2 characters long" }),
-    message: z.string().trim().min(10, { message: "Message must be at least 10 characters long" }),
-});
+// const formSchema = z.object({
+//     email: z
+//         .string()
+//         .trim().email({ message: "Must be a valid email" }),
+//     name: z.string().trim().min(2, { message: "Name must be at least 2 characters long" }),
+//     message: z.string().trim().min(10, { message: "Message must be at least 10 characters long" }),
+// });
 
 const Contact = () => {
 
-    const handleSendEmail = async () => {
-        try {
-            const response = await fetch("/api/send", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    from: form.values.email,
-                    to: "farooq.rohaan@gmail.com",
-                    name: form.values.name,
-                    subject: `Contact Form - Message from ${form.values.name}`,
-                    message: form.values.message,
-                }),
-            });
+    // const handleSendEmail = async () => {
+    //     try {
+    //         const response = await fetch("/api/send", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({
+    //                 from: form.values.email,
+    //                 to: "farooq.rohaan@gmail.com",
+    //                 name: form.values.name,
+    //                 subject: `Contact Form - Message from ${form.values.name}`,
+    //                 message: form.values.message,
+    //             }),
+    //         });
 
-            if (response.ok) {
-                alert("Email sent successfully!");
-            } else {
-                console.error('Error sending email');
-            }
-            form.reset();
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    //         if (response.ok) {
+    //             alert("Email sent successfully!");
+    //         } else {
+    //             console.error('Error sending email');
+    //         }
+    //         form.reset();
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
 
-    const form = useForm({
-        initialValues: {
-            email: "",
-            name: "",
-            message: "",
-        },
-        validate: (values) => zodResolver(formSchema)(values),
-    });
-    const formSec: React.CSSProperties ={
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: '2rem',  
-    }
-    const formStyle: React.CSSProperties = {
-        width: '50%',
-        display: 'flex',
-        // border: '2px solid white',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    };
-    const labelStyle: React.CSSProperties = {
-        color: 'white',
-        fontSize: '16px',
-        marginBottom: '5px',
-    };
+    // const form = useForm({
+    //     initialValues: {
+    //         email: "",
+    //         name: "",
+    //         message: "",
+    //     },
+    //     validate: (values) => zodResolver(formSchema)(values),
+    // });
+    // const formSec: React.CSSProperties ={
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     marginTop: '2rem',  
+    // }
+    // const formStyle: React.CSSProperties = {
+    //     width: '50%',
+    //     display: 'flex',
+    //     // border: '2px solid white',
+    //     flexDirection: 'column',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    // };
+    // const labelStyle: React.CSSProperties = {
+    //     color: 'white',
+    //     fontSize: '16px',
+    //     marginBottom: '5px',
+    // };
 
-    const inputStyle: React.CSSProperties = {
-        width: '100%',
-        height: '40px',
-        padding: '10px',
-        fontSize: '16px',
-        color: 'white',
-        backgroundColor: 'white !important' ,
-        borderRadius: '5px',
-        border : 'solid white 2px',
-        outline: 'none',
-    };
+    // const inputStyle: React.CSSProperties = {
+    //     width: '100%',
+    //     height: '40px',
+    //     padding: '10px',
+    //     fontSize: '16px',
+    //     color: 'white',
+    //     backgroundColor: 'white !important' ,
+    //     borderRadius: '5px',
+    //     border : 'solid white 2px',
+    //     outline: 'none',
+    // };
 
     return (
         <AppWrapper>
 
-            <FloatingNav navItems={navItems} className="max-w-full mx-auto" />
+            {/* <FloatingNav navItems={navItems} className="max-w-full mx-auto" />
             <Hero />
             <InstagramStoryViewer />
             <div style={formSec}>
@@ -177,7 +177,7 @@ const Contact = () => {
                 </form>
             </div>
 
-            <Footer />
+            <Footer /> */}
         </AppWrapper>
 
     )
