d2c887c5c6012ec811fe62c668b2cdce1656c6cb
Haseeb Saqib <114069880+HaseebSaqib01010@users.noreply.github.com>
2024-12-19 12:51:53 +0500

    comment twMerge from lib/utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs));
+// }
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return clsx(inputs); // Remove twMerge temporarily
 }
